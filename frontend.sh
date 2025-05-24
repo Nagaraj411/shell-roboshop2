0f0ec2308cb66e7361bfb2026b9ebeb8cd41fc70
Nagaraj <nagarajuande255@gmail.com>
2025-05-24 10:04:35 +0530

    shell-roboshop2

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
-source ./common.sh
-check_root
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
 dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Nginx module disable"
@@ -17,6 +48,7 @@ VALIDATE $? "Nginx service enable"
 
 systemctl start nginx  &>>$LOG_FILE
 VALIDATE $? "Nginx service start"
+
 
 rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
 VALIDATE $? "Nginx default content removal"
@@ -40,6 +72,3 @@ systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Nginx service restart"
 
 print_time
-
-
-
