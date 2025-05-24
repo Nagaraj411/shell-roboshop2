05c26d9f0fad02ec5748febca69d1116de2984c7
Nagaraj <nagarajuande255@gmail.com>
2025-05-24 08:32:36 +0530

    shell-roboshop2

--- catalogue.sh
+++ catalogue.sh
@@ -1,68 +1,11 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+source ./common.sh
+app_name=catalogue
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-# check the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
-
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
-
-dnf module disable nodejs -y    &>>$LOG_FILE
-VALIDATE $? "NodeJS module disable"
-
-dnf module enable nodejs:20 -y  &>>$LOG_FILE
-VALIDATE $? "NodeJS module enable"
-
-dnf install nodejs -y   &>>$LOG_FILE
-VALIDATE $? "NodeJS installation"
-
-# check if the roboshop user is already created or not
-id roboshop
-if [ $? -ne 0 ]
-then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop    &>>$LOG_FILE
-    VALIDATE $? "Creating roboshop system user"
-else
-    echo -e "System user roboshop already created ... $Y SKIPPING $N"
-fi
-
-mkdir -p /app
-VALIDATE $? "app folder creation"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading Catalogue"
-
-rm -rf /app/* #remove all the files in app folder
-cd /app       #go to app folder
-unzip /tmp/catalogue.zip    &>>$LOG_FILE
-VALIDATE $? "catalogue zip file extraction"
+check_root
+nodejs_setup
+app_setup
 
 npm install &>>$LOG_FILE
 VALIDATE $? "npm install"
@@ -95,7 +38,4 @@ fi
 mongosh --host mongodb.devops84.shop
 VALIDATE $? "MongoDB connection"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+print_time
--- common.sh
+++ common.sh
@@ -1,42 +1,75 @@
 #!/bin/bash
 
-systemctl
-    START_TIME=$(date +%s)
-    USERID=$(id -u)
-    R="\e[31m"
-    G="\e[32m"
-    Y="\e[33m"
-    N="\e[0m"
-    LOGS_FOLDER="/var/log/roboshop-logs"
-    SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-    LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-    SCRIPT_DIR=$PWD
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
-    mkdir -p $LOGS_FOLDER
-    echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-   check_root_user(){
-        if [ $USERID -ne 0 ]
-        then
-            echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-            exit 1 #give other than 0 upto 127
-        else
-            echo "You are running with root access" | tee -a $LOG_FILE
-        fi
-   }
-    # validate functions takes input as exit status, what command they tried to install
-    VALIDATE(){
-        if [ $1 -eq 0 ]
-        then
-            echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-        else
-            echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-            exit 1
-        fi
-    }
+app_setup(){
+    id roboshop &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop    &>>$LOG_FILE
+        VALIDATE $? "Creating roboshop system user"
+    else
+        echo -e "System user roboshop already created ... $Y SKIPPING $N"
+    fi
 
-    print_time(){
-        END_TIME=$(date +%s)
-        TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-        echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE  
-    }
+    mkdir -p /app
+    VALIDATE $? "app folder creation"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "Downloading $app_name"
+
+    rm -rf /app/* #remove all the files in app folder
+    cd /app       #go to app folder
+    unzip /tmp/$app_name.zip    &>>$LOG_FILE
+    VALIDATE $? "$app_name zip file extraction"
+}
+
+nodejs_setup(){
+    dnf module disable nodejs -y    &>>$LOG_FILE
+    VALIDATE $? "NodeJS module disable"
+
+    dnf module enable nodejs:20 -y  &>>$LOG_FILE
+    VALIDATE $? "NodeJS module enable"
+
+    dnf install nodejs -y   &>>$LOG_FILE
+    VALIDATE $? "NodeJS installation"
+}
+
+check_root(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+        exit 1 #give other than 0 upto 127
+    else
+        echo "You are running with root access" | tee -a $LOG_FILE
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+print_time(){
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+    echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE  
+
+}
